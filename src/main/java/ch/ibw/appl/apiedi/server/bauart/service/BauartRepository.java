c1ebe8ae5e27affbe3586d4129403f2e414da80f
vinkje <spescha_marc@yahoo.de>
2020-06-23 19:46:48 +0200

    Stash conflikt

---
+++ src/main/java/ch/ibw/appl/apiedi/server/bauart/service/BauartRepository.java
@@ -0,0 +1,15 @@
+package ch.ibw.appl.apiedi.server.bauart.service;
+
+import ch.ibw.appl.apiedi.server.angebot.model.Angebot;
+import ch.ibw.appl.apiedi.server.behandlungen.model.ModelId;
+import ch.ibw.appl.apiedi.server.shared.service.Repository;
+
+import java.util.List;
+
+interface AngebotRepository<T extends Angebot> extends Repository<T> {
+  List<T> all();
+  ModelId add(T obj);
+  T get(int id);
+  T findByDescription(String description);
+
+}
--- src/main/resources/META-INF/testdata.sql
+++
@@ -1,8 +0,0 @@
-TRUNCATE TABLE behandlung;
-INSERT INTO behandlung (date, angebotid, anzahl)
-VALUES 
-('2015-08-08', 1, 4),
-('2015-08-09', 2, 5),
-('2015-08-10', 3, 6)
-;
-
